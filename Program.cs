73356275a4d105577919736d9c3423753d3e05ce
Igna <ignacio.marcovecchio@istea.com.ar>
2025-12-01 23:00:34 -0300

    Configurar PostgreSQL de Render

--- Program.cs
+++ Program.cs
@@ -28,13 +28,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
